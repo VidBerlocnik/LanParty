533ee554a6eca40135cc49f10418007b4af709f0
VidBerlocnik <vid.berlocnik@scv.si>
2021-03-18 15:50:28 +0100

    Added Registration form with GUI components.

---
+++ src/main/java/Registration.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Registration {
+    private JPanel panel;
+    private JButton registerButton;
+    private JPasswordField passwordField1;
+    private JTextField usernameTextField;
+    private JLabel label1;
+    private JLabel label3;
+    private JTextField emailTextField;
+    private JLabel label2;
+    private JRadioButton genderRadioButton1;
+    private JRadioButton genderRadioButton2;
+    private JLabel label4;
+    private JLabel label5;
+    private JComboBox dayComboBox;
+    private JComboBox yearComboBox;
+    private JComboBox monthComboBox;
+    private JLabel label6;
+    private JLabel label7;
+    private JLabel label8;
+
+
+    public Registration(){
+        JFrame jframe = new JFrame("LAN Party registration");
+        jframe.setContentPane(panel);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.pack();
+        jframe.setSize(600,400); // change these
+        jframe.setVisible(true);
+        setActionListeners();
+    }
+
+    private void setActionListeners() {
+        registerButton.addActionListener(e -> {
+
+        });
+    }
+}
