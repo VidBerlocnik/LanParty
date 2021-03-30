f24cca7ae335f51dbd61dbb108cf6d0605c965ae
VidBerlocnik <vid.berlocnik@scv.si>
2021-03-30 11:54:37 +0200

    Created new class Party

---
+++ src/main/java/Party.java
@@ -0,0 +1,16 @@
+public class Party {
+    Integer Id;
+    String Name;
+    String Date;
+
+    public Party(Integer id, String name, String date){
+        Id = id;
+        Name = name;
+        Date = date;
+    }
+    public Party(){
+        Id = null;
+        Name = null;
+        Date = null;
+    }
+}
