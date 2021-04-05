06370b49717596e2b5bff17a73f7bd0e1448db1f
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-05 17:49:11 +0200

    Created CreateTeam form and added components to GUI.

---
+++ src/main/java/CreateTeam.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class CreateTeam {
+    private JPanel panel;
+    private JTextField teamNameTextField;
+    private JLabel label1;
+    private JButton createTeamButton;
+    private JButton addLogoButton;
+    private JLabel label2;
+    private JLabel imagePathLabel;
+}
