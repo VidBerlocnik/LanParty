56ad1cc1ce9bbd29c6dfea7c0006dbbca3c83759
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-06 12:34:07 +0200

    Created new form Statistics.

---
+++ src/main/java/Statistics.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Statistics {
+    private JPanel panel;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel numberOfPartiesLabel;
+    private JLabel numberOfTeamsLabel;
+    private JLabel label3;
+    private JLabel averageTeamsLabel;
+    private JButton BackButton;
+}
