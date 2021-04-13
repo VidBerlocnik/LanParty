6dfbabf338583c9309b48729e7570e997c5fbc9e
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-13 12:41:48 +0200

    Select image window opens on "Add image" button click

--- src/main/java/CreateTeam.java
+++ src/main/java/CreateTeam.java
@@ -2,10 +2,13 @@ import javax.swing.*;
 import javax.imageio.ImageIO;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.crypto.Data;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CreateTeam {
@@ -21,6 +24,9 @@ public class CreateTeam {
     private JFrame jframe = new JFrame("LAN Create team");
     private User user;
 
+    public static String fileName;
+    public boolean fileIfDelete = true;
+
     public CreateTeam(){
         jframe.setContentPane(panel);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,6 +35,13 @@ public class CreateTeam {
         jframe.setVisible(true);
         fillComboBox();
         setActionListeners();
+
+        jframe.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                onExit();
+                jframe.dispose();
+            }
+        });
     }
 
     public CreateTeam(User userr){
@@ -56,6 +69,46 @@ public class CreateTeam {
                 }
             }
         });
+
+        addLogoButton.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                    "JPG, JPEG, and PNG Images", "jpg", "jpeg", "png");
+            chooser.setFileFilter(filter);
+            int returnVal = chooser.showOpenDialog(panel);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                System.out.println("You chose to open this file: "
+                        + file.getName());
+                BufferedImage image;
+                try {
+                    image = ImageIO.read(file);
+                    ImageIO.write(image, "jpg",new File("src\\img\\" + file.getName()));
+                    ImageIO.write(image, "jpeg",new File("src\\img\\" + file.getName()));
+                    ImageIO.write(image, "png",new File("src\\img\\" + file.getName()));
+                    fileName = file.getName();
+                    fileIfDelete = true;
+                } catch (IOException ex) {
+                    Logger.getLogger(CreateTeam.class.getName()).log(Level.SEVERE, null, ex);
+
+                    //izpis errorja ce ne zberes slike
+                    JOptionPane.showMessageDialog(panel,
+                            "Niste izbrali slike!",
+                            "Error!",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+    }
+
+    public void onExit() {
+        if (fileIfDelete)
+        {
+            File file = new File("%USERPROFILE%" + fileName);
+            System.out.print(file);
+            //file.delete();
+        }
     }
 
     private void fillComboBox(){
