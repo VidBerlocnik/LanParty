f84eb41c239faca696ea851bffdf765bc6d873e1
VidBerlocnik <vid.berlocnik@scv.si>
2021-04-20 12:25:49 +0200

    Created new form ImageDisplay that shows team's logo.

---
+++ src/main/java/ImageDisplay.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+public class ImageDisplay {
+    private JPanel panel;
+    private JLabel lPhoto;
+    private JFrame jframe = new JFrame("Image display");
+
+    public ImageDisplay(String teamName){
+        jframe.setContentPane(panel);
+        jframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jframe.pack();
+        jframe.setSize(600,400); // change these
+        jframe.setVisible(true);
+        setImage(teamName);
+    }
+
+    public static Connection connect() {
+        String url = "jdbc:postgresql://ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d7cv2ut5vo5cij";
+        String user = "ldxwthgxjzmesj";
+        String password = "a52368b3eb287186c1da8c9123ade07b361bc07ab097fbe7463a56188c8bbb0c";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            Messages.databaseConnectionFailed("d7cv2ut5vo5cij", e.getMessage());
+        }
+        return con;
+    }
+
+    public void setImage(String teamName){
+        String cmd = "SELECT logo FROM teams WHERE(name = '" + teamName + "');";
+
+        try (Connection con = connect();
+             Statement st = con.createStatement();
+             ResultSet set = st.executeQuery(cmd)) {
+
+            String image = null;
+            while (set.next()) {
+                image = set.getString("logo");
+            }
+
+            ImageIcon slika = new ImageIcon("src\\img\\" + image);
+            lPhoto.setIcon(slika);
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
