fd0aaa49973cde06aeb57e409385eb70bdea449f
VidBerlocnik <vid.berlocnik@scv.si>
2021-05-04 13:12:26 +0200

    Updated gitignore file and Database file.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -10,14 +10,15 @@ import java.util.ArrayList;
 
 public class Database {
     public static Connection connect() {
-        String url = "jdbc:postgresql://ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d7cv2ut5vo5cij";
-        String user = "ldxwthgxjzmesj";
-        String password = "a52368b3eb287186c1da8c9123ade07b361bc07ab097fbe7463a56188c8bbb0c";
+        DB db = new DB();
+        String url = db.url;
+        String user = db.user;
+        String password = db.password;
         Connection con = null;
         try {
             con = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            Messages.databaseConnectionFailed("d7cv2ut5vo5cij", e.getMessage());
+            Messages.databaseConnectionFailed(db.database, e.getMessage());
         }
         return con;
     }
